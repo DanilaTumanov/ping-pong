fbdc1eb2661b24a18a21be04739493c8f78dcf38
DanT <danilatumanov@gmail.com>
2020-09-23 23:59:30 +0300

    initial + прототип игрового поля

---
+++ PingPong/Assets/Scripts/IGameFieldObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGameFieldObject : IBouncable
+{
+
+    Vector3 Position { get; }
+    Bounds Bounds { get; }
+    
+    void OnOut();
+    void OnGameFieldReset();
+
+}
